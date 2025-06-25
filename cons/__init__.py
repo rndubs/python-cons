03bf56e035a6372e5166c64cc65a8e92f60d7544
Ryan Whitmore <whitmore5@llnl.gov>
2025-06-24 21:45:48 -0700

    Remove the versioneer git tag dynamic versioning in favor of setuptools-scm. Update the pre-commit hooks to use a python 3.13 compatible version of autoflake.

--- cons/__init__.py
+++ cons/__init__.py
@@ -1,6 +1,6 @@
+from importlib.metadata import version
+
 from . import unify  # noqa: F401
-from ._version import get_versions
 from .core import car, cdr, cons  # noqa: F401
 
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = version("cons")
